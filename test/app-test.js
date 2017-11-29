c547aa9453eb2a1566f6049c98729b293a7816e3
iPolyomino <dpm26508@kwansei.ac.jp>
2017-11-29 22:58:32 +0900

    アプリケーション用のテストを追加

---
+++ test/app-test.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const Nightmare = require('nightmare');
+const app = require('../app');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Express Server Test', () => {
+    it('Should return status code 200', (done) => {
+        chai.request(app).get('/').end( (err, res) => {
+            expect(err).to.be.null;
+            expect(res).to.have.status(200);
+            done();
+        });
+    });
+});
+
+describe('Application Test', () => {
+    const url = 'http://localhost:3000/';
+
+    it('Should have object named "box".', (done) => {
+        const nightmare = new Nightmare();
+        nightmare.goto(url).exists('#box').then( (exists) => {
+            console.log(exists);
+            expect(exists).to.be.true;
+            done();
+        });
+    });
+});
