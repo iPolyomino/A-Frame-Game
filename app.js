62acaf6dbf2320a0762026212d87fa6b795dcb0d
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2019-02-17 16:41:07 +0900

    add prettier

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.static('docs'));
+app.use(express.static("docs"));
 
-app.get('/', (req, res) => {
-    res.render('index.html');
+app.get("/", (req, res) => {
+  res.render("index.html");
 });
 
 app.listen(3000, () => {
-    console.log('server runnig at http://localhost:3000/');
+  console.log("server runnig at http://localhost:3000/");
 });
 
 module.exports = app;
